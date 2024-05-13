ffafabfbb573491c04fae27a3eeb2c225d683e38
sahith <sahithkatta11@gmail.com>
2024-05-13 15:30:32 +0530

    as per jira-1001 updated all the changes and pushed the changes to new branch

--- src/main/java/com/nt/DAO/IEmployeeDAO.java
+++ src/main/java/com/nt/DAO/IEmployeeDAO.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import com.nt.model.Employee;
 
+/**
+ *
+ *
+ *
+ * ksdfsdklf
+ */
 public interface IEmployeeDAO
 {
 	public List<Employee> getEmployeesByDesgs(String desg1,String desg2,String desg3) throws Exception;
